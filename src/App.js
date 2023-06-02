91687a1d049f8580818372886ba308e15ea2cfc6
Soter <tirsoclarineteharo@gmail.com>
2023-06-02 19:59:54 +0200

    scroll v2

--- src/App.js
+++ src/App.js
@@ -25,13 +25,6 @@ let nota4
 let nota5
 let nota6
 
-TopScroll = window.pageYOffset || document.documentElement.scrollTop;
-LeftScroll = window.pageXOffset || document.documentElement.scrollLeft,
-
-// if scroll happens, set it to the previous value
-window.onscroll = function() {
-window.scrollTo(LeftScroll, TopScroll);
-        };
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
