56b31d55284d58b238ac2a7e16002fd5bcce51b1
Soter <tirsoclarineteharo@gmail.com>
2023-06-03 11:02:03 +0200

    scroll arreglado

--- src/App.js
+++ src/App.js
@@ -176,8 +176,8 @@ if (orientation === "Portrait") {
     return(<h1 id="apaisado"> Pon el teléfono en apaisado</h1>)
 } else {
 
-    window.scrollTo(0,0)
-    window.onscroll = function() {window.scrollTo(0,0)}
+    window.scrollTo(0,5000)
+    window.onscroll = function() {window.scrollBy(0,0)}
 
 if (!ready) {
     return(
